f1666289040f435f5ec3ff24908a5d9d04468d38
Gisele <gisesonia@gmail.com>
2025-06-13 19:33:37 -0300

    feat(produtos-service): cria ProdutoController com endpoint base /produtos

--- produtos-service/src/main/java/com/bancoprodutos/produtos_service/adapter/in/ProdutoController.java
+++ produtos-service/src/main/java/com/bancoprodutos/produtos_service/adapter/in/ProdutoController.java
@@ -1,4 +1,29 @@
 package com.bancoprodutos.produtos_service.adapter.in;
 
+import com.bancoprodutos.produtos_service.core.usecase.ProdutoService;
+import com.bancoprodutos.produtos_service.dto.request.ProdutoRequest;
+import com.bancoprodutos.produtos_service.dto.response.ProdutoResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/produtos")
 public class ProdutoController {
+
+    private final ProdutoService produtoService;
+
+    public ProdutoController(ProdutoService produtoService) {
+        this.produtoService = produtoService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProdutoResponse>> listar() {
+        return ResponseEntity.ok(produtoService.listarProdutos());
+    }
+
+    @PostMapping
+    public ResponseEntity<ProdutoResponse> criar(@RequestBody ProdutoRequest request) {
+        return ResponseEntity.ok(produtoService.salvarProduto(request));
+    }
 }
